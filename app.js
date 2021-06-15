663bb64cffc0cccc4345a6e0a67fa3b8a11602ac
ThaheemDev <thaheem001@gmail.com>
2021-06-15 11:43:22 +0530

    replace var with const

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var middleware = require('./middleware/index');
+const createError = require('http-errors'),
+          express = require('express'),
+          path = require('path'),
+          cookieParser = require('cookie-parser'),
+          logger = require('morgan'),
+          middleware = require('./middleware/index'),
+          indexRouter = require('./routes/index'),
+          bodyParser = require('body-parser'),
+          db = require("./models");
 
-var indexRouter = require('./routes/index');
 
-const db = require("./models");
 db.sequelize.sync();
 
-var app = express();
+const app = express();
+
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,21 +35,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 middleware(app)
 
-// app.use('/', indexRouter);
 app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
