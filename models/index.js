d1a0fab03b50b1867f371e00bebd87102f9912eb
ThaheemDev <thaheem001@gmail.com>
2021-06-15 11:36:20 +0530

    changes in model index.js to import all model file automatically

--- models/index.js
+++ models/index.js
@@ -1,13 +1,12 @@
 const config = require("../config/config.js");
 const Sequelize = require("sequelize");
-
+const fs = require('fs');
 const dbConfig = config.database;
-
-const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
+const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {  
   host: dbConfig.host,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
-
+  logging: false,
   pool: {
     max: dbConfig.pool.max,
     min: dbConfig.pool.min,
@@ -15,12 +14,18 @@ const sequelize = new Sequelize(dbConfig.db, dbConfig.user, dbConfig.password, {
     idle: dbConfig.pool.idle
   }
 });
-
 const db = {};
-
 db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+fs.readdir('./models', (err, files) => {
+  files.forEach(file => {
+    if (file != 'index.js') {
+      let splitFile = file.split('.js');
+      let name = splitFile[0];
+      let filePath = `./${file}`
+      db[name] = require(filePath)(sequelize, Sequelize);
+    }
+  });
+});
 
-db.user = require("./user.js")(sequelize, Sequelize);
 
 module.exports = db;
