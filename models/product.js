ce49ade4beda575d959dfd6c5884610e313c115a
ThaheemDev <thaheem001@gmail.com>
2021-07-07 19:07:29 +0530

    added product model

---
+++ models/product.js
@@ -0,0 +1,83 @@
+const { v4: uuidv4 } = require('uuid');
+const db = require('.');
+
+module.exports = (sequelize, Sequelize) => {
+    const Product = sequelize.define("product", {
+        external_id: {
+            type: Sequelize.STRING,
+            unique: true,
+            allowNull: false,
+            set(val) {
+                let uuid = uuidv4();
+                this.setDataValue('external_id', uuid);
+            }
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 255],
+                    msg: "Name can not be greater than 255"
+                },
+                notNull: {
+                    msg: 'Name is required'
+                }
+            }
+        },
+        short_description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 255],
+                    msg: "Name can not be greater than 255"
+                },
+                notNull: {
+                    msg: 'Name is required'
+                }
+            }
+        },
+
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            validate: {
+                len: {
+                    args: [1, 255],
+                    msg: "Name can not be greater than 255"
+                },
+                notNull: {
+                    msg: 'Name is required'
+                }
+            }
+        },
+
+        language: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+
+        price: {
+            type: Sequelize.INTEGER,
+            allowNull: false
+        },
+
+        active: {
+            type: Sequelize.BOOLEAN
+        },
+        category: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        sub_category: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        brand: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+    });
+    return Product;
+};
